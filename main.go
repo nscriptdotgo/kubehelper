28ccb3909a898bf989d9ea91c668022164eba701
Noah Dlugoszewski <noahjdlugoszewski@gmail.com>
2021-12-10 17:46:12 -0600

    get count endpoint and basic health check. learning golang.

--- main.go
+++ main.go
@@ -21,6 +21,11 @@ import (
 	"k8s.io/client-go/util/retry"
 )
 
+type HealthCheckResponse struct {
+	Status string `json:"status"`
+	StatusCode int32 `json:"status_code"`
+}
+
 type ScaleResponse struct {
 	Message	string `json:"message"`
 }
@@ -29,6 +34,11 @@ type ScaleRequest struct {
 	Deployment string `json:"deployment"`
 	Namespace  string `json:"namespace"`
 	Replicas   int32  `json:"replicas"`
+}
+
+type PodCountRequest struct {
+	Deployment string `json:"deployment"`
+	Namespace  string `json:"namespace"`
 }
 
 func int32Ptr(i int32) *int32 { return &i }
@@ -55,43 +65,78 @@ func healthCheck(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func scaleDeployment(w http.ResponseWriter, r *http.Request) {
+func podCount(w http.ResponseWriter, r *http.Request) {
 	clientset := getKubernetesClientset()
-	var patchUpdateObject ScaleRequest
+	var podCountRequest PodCountRequest
+
 	reqBody, err := ioutil.ReadAll(r.Body)
 	defer r.Body.Close()
 	if err != nil {
 		fmt.Fprintf(w, "Please ensure your request is properly formed.")
 	}
-	json.Unmarshal(reqBody, &patchUpdateObject)
-
-	log.Print(patchUpdateObject)
+	json.Unmarshal(reqBody, &podCountRequest)
 	if err != nil {
 		fmt.Fprintf(w, "Something happened during unmarshaling")
 	}
+	log.Print(podCountRequest)
 
 	retryErr := retry.RetryOnConflict(retry.DefaultRetry, func() error {
 		// Retrieve the latest version of Deployment before attempting update
 		// RetryOnConflict uses exponential backoff to avoid exhausting the apiserver
-		// result, getErr := deploymentsClient.Get(context.TODO(), patchUpdateObject.Deployment, metav1.GetOptions{})
-		result, getErr := clientset.AppsV1().Deployments(patchUpdateObject.Namespace).Get(context.TODO(), patchUpdateObject.Deployment, metav1.GetOptions{})
+		// result, getErr := deploymentsClient.Get(context.TODO(), patchDeploymentUpdate.Deployment, metav1.GetOptions{})
+		result, getErr := clientset.AppsV1().Deployments(podCountRequest.Namespace).Get(context.TODO(), podCountRequest.Deployment, metav1.GetOptions{})
+		if getErr != nil {
+			panic(fmt.Errorf("Failed to get latest version of Deployment: %v", getErr))
+		}
+		log.Print(result.Spec)
+		json.NewEncoder(w).Encode(result.Spec.Replicas)
+		return getErr
+	})
+	if retryErr != nil {
+		panic(fmt.Errorf("Update failed: %v", retryErr))
+	}
+
+}
+
+func scaleDeployment(w http.ResponseWriter, r *http.Request) {
+	clientset := getKubernetesClientset()
+	var patchDeploymentUpdate ScaleRequest
+	var scaleResponseMessage ScaleResponse
+	reqBody, err := ioutil.ReadAll(r.Body)
+	defer r.Body.Close()
+	if err != nil {
+		fmt.Fprintf(w, "Please ensure your request is properly formed.")
+	}
+	json.Unmarshal(reqBody, &patchDeploymentUpdate)
+	if err != nil {
+		fmt.Fprintf(w, "Something happened during unmarshaling")
+	}
+	log.Print(patchDeploymentUpdate)
+
+	retryErr := retry.RetryOnConflict(retry.DefaultRetry, func() error {
+		// Retrieve the latest version of Deployment before attempting update
+		// RetryOnConflict uses exponential backoff to avoid exhausting the apiserver
+		// result, getErr := deploymentsClient.Get(context.TODO(), patchDeploymentUpdate.Deployment, metav1.GetOptions{})
+		result, getErr := clientset.AppsV1().Deployments(patchDeploymentUpdate.Namespace).Get(context.TODO(), patchDeploymentUpdate.Deployment, metav1.GetOptions{})
 		if getErr != nil {
 			panic(fmt.Errorf("Failed to get latest version of Deployment: %v", getErr))
 		}
 
-		result.Spec.Replicas = int32Ptr(patchUpdateObject.Replicas)
-		_, updateErr := clientset.AppsV1().Deployments(patchUpdateObject.Namespace).Update(context.TODO(), result, metav1.UpdateOptions{})
+		result.Spec.Replicas = int32Ptr(patchDeploymentUpdate.Replicas)
+		_, updateErr := clientset.AppsV1().Deployments(patchDeploymentUpdate.Namespace).Update(context.TODO(), result, metav1.UpdateOptions{})
 		return updateErr
 	})
 	if retryErr != nil {
 		panic(fmt.Errorf("Update failed: %v", retryErr))
 	}
-	json.NewEncoder(w).Encode("Deployment scaled")
+	scaleResponseMessage.Message = "Deployment Scaled"
+	// json.Marshal()
+	json.NewEncoder(w).Encode(scaleResponseMessage)
 }
 
 func main() {
 	http.HandleFunc("/api/v1/deployment/scale", scaleDeployment)
+	http.HandleFunc("/api/v1/deployment/podcount", podCount)
 	http.HandleFunc("/health", healthCheck)
-	// http.HandleFunc("/api/v1/deployment/list", getDeployment).Methods("GET")
 	log.Fatal(http.ListenAndServe(":8081", nil))
 }
